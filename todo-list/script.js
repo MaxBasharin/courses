0aaa5220f70451968cf62c95bb26638cc7a4aefe
MaxBasharin <maxbasharinweb@gmail.com>
2022-10-20 20:28:01 +0300

    завершил рзработку

--- todo-list/script.js
+++ todo-list/script.js
@@ -9,23 +9,25 @@ const tasks = []
 // Отслеживаем клик по кнопке Добавить задачу
 dom.add.onclick = () => {
 	const newTaskText = dom.new.value // берет значение поля с id new
-	if (newTaskText && isNotHaveTask(newTaskText, tasks)) { // если в task что-то есть, то выполнять функцию addTask
+	if (newTaskText && isNotHaveTask(newTaskText, tasks)) // если в task что-то есть, то выполнять функцию addTask
+	{
 		addTask(newTaskText, tasks)
 		dom.new.value = '' // очищет поле input
+		tasksRender(tasks)
+		console.log(tasks)
 	}
 }
 
 // Функция добавления задачи
 
-function addTask(textValue) {
+function addTask(text, list) {
 	const timestamp = Date.now() // date.now берёт значение даты в милисекундах
 	const task = {
 		id: timestamp,
-		text: textValue,
-		isComplate: false
+		text: text,
+		isComplete: false
 	}
-	tasks.push(task) // push метод добавления 
-	console.log(tasks)
+	list.push(task) // push метод добавления 
 }
 
 // проверка существования задачи в массиве задач
@@ -41,4 +43,73 @@ function isNotHaveTask(text, list) {
 	});
 
 	return isNotHave
+}
+
+// функция вывода списка задач на страницу
+function tasksRender(list) {
+	let htmlList = ''
+
+	list.forEach((task) => {
+		const cls = task.isComplete
+			? // если task.isComplete = true то 
+			'todo__task todo__task_complate'
+			: // если task.isComplete = false то
+			'todo__task'
+			const checked = task.isComplete ? 'checked' : ''
+
+		const taskHtml = `
+		<div class="${cls}" id="${task.id}">
+			<label  class="todo__checkbox" >
+				<input type="checkbox" ${checked}><div class="todo__checkbox-div"></div>
+			</label>
+			<div class="todo__task-text">
+				${task.text}
+			</div>
+			<button type="button" class="todo__task-del">
+			</button>
+	</div>
+		`
+
+		htmlList = htmlList + taskHtml
+	})
+
+	dom.tasks.innerHTML = htmlList
+}
+
+// Отслеживаем клик по checkbox задачи 
+dom.tasks.onclick = (event) => { // определяет по какому элементу произошёл клик 
+	const target = event.target
+	const isCheckboxEl = target.classList.contains('todo__checkbox-div')
+	const isDeleteEL = target.classList.contains('todo__task-del')
+	
+	if (isCheckboxEl) { //parentNode поднимает на уровень выше
+		const task = target.parentElement.parentElement
+		const taskId = task.getAttribute('id')
+		changeTaskStatus(taskId, tasks)
+		tasksRender(tasks) // перерисовка
+	}
+	if (isDeleteEL) {
+		const task = target.parentElement
+		const taskId = task.getAttribute('id')
+		deleteTask(taskId, tasks)
+		tasksRender(tasks) // перерисовка
+	}
+}
+
+// функция изменения статуса задачи
+function changeTaskStatus(id, list) {
+	list.forEach((task) => {
+		if (task.id == id) {
+			task.isComplete = !task.isComplete
+		}
+	})
+}
+
+// функция удаления задачи 
+function deleteTask(id, list) {
+	list.forEach((task, idx) => {
+		if (task.id == id) {
+			list.splice(idx, 1) // вырезать эелемнт массива 
+		}
+	})
 }
